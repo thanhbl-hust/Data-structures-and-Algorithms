efda795a4b75e0fcd2e5e21bdc40e80c3d55a08a
thanhbl-hust <lamthanhbui02@gmail.com>
2023-11-20 21:53:15 +0700

    3

---
+++ teachHN/6.py
@@ -0,0 +1,18 @@
+# n = int(input("n = "))
+
+# uoc = 0
+
+# for i in range(2, n):
+#     if n % i == 0:
+#         uoc += 1
+
+# if uoc > 0:
+#     print("n is not snt!")
+# else:
+#     print("n is snt")
+
+for i in range(1, 10):
+
+    if i > 5:
+        break 
+    print(i)
