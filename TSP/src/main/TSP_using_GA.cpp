2d805cc94e48bb66c0267f0c49e095581ceb2609
thanhbl-hust <lamthanhbui02@gmail.com>
2024-01-09 00:12:57 +0700

    test c++ code

---
+++ TSP/src/main/TSP_using_GA.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+//declare parameters;
+int stt;
+int cities = 500;
+int generation = 100;
+float crossover_rating = 0.9;
+float mutation_rating = 0.1;
+vector<vector<float>> distance_matrix;
+vector<vector<float>> coordinate(cities, vector<float> (2));
+
+
+void input(){
+
+    //input number of cities;
+    cin >> cities;
+
+    //create distance vector;
+    vector<vector<float>> distance(cities, vector<float> (cities));
+
+    //cout << coordinate.size() << endl;
+    //resize coordinate matrix for memory saving;
+    coordinate.resize(cities);
+    //cout << coordinate.size() << endl;
+
+     
+    //input coordinate file;
+    for(int i = 0; i < cities; i++){
+        cin >> stt;
+        for(int j = 0; j < 2; j++){
+            cin >> coordinate[i][j];
+        }
+    }
+
+    //get distance matrix;
+    for(int i = 0; i < cities; i++){
+        for(int j = 0; j < cities; j++){
+            distance[i][j] = sqrt(pow((coordinate[i][0] - coordinate[j][0]), 2) + pow(coordinate[j][0] - coordinate[j][1], 2));
+        }
+    }
+
+    distance_matrix = distance;
+}
+
+int main(){
+
+
+    freopen("test_data.txt", "r", stdin);
+
+    input();
+
+    
+    
+    //RESULT;
+
+
+    cout << "Number of cities: " << cities << endl;
+    cout << "Generation: " << generation << endl;
+    cout << "Crossover rating: " << crossover_rating << endl;
+    cout << "Mutation rating: " << mutation_rating << endl;
+    cout << "Size of distance matrix: " << endl;
+    cout << "-- Row: " << distance_matrix.size() << endl;
+    cout << "-- Column: " << distance_matrix[0].size() << endl;
+
+
+    //test_coordinate;
+    // for(int i = 0; i < cities; i++){
+    //     for(int j = 0; j < 2; j++){
+    //         cout << coordinate[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    //test_distance_matrix_file;
+    // for(int i = 0; i < distance_matrix.size(); i++){
+    //     for(int j = 0; j < distance_matrix[i].size(); j++){
+    //         cout << distance_matrix[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    return 0;
+}
--- TSP/test.cpp
+++ TSP/test.cpp
@@ -1,17 +1,5 @@
-#include <iostream>
-#include <algorithm>
-#include <random>
-#include <chrono>
+#include<bits/stdc++.h>
 
 int main() {
-
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::shuffle(arr, arr + n, std::default_random_engine(seed));
-
-    for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
-
     return 0;
 }
